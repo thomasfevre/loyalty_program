e80f6d9ee4f5d5a58be664c74c0de2c779a19b10
thomasfevre <thomas.fevre@viacesi.fr>
2025-04-13 10:51:26 +0200

    added init_token & mint_token methods

--- anchor/programs/loyalty_pay/src/lib.rs
+++ anchor/programs/loyalty_pay/src/lib.rs
@@ -1,8 +1,17 @@
 use crate::constants::USDC_MINT;
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::pubkey;
-use anchor_spl::token::{transfer, Token, TokenAccount, Transfer};
-use anchor_spl::token_interface::Mint;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::Mint,
+    token::{mint_to, Mint, MintTo, transfer, Token, TokenAccount, Transfer}
+    metadata::{
+        create_metadata_accounts_v3,
+        mpl_token_metadata::types::DataV2,
+        CreateMetadataAccountsV3, 
+        Metadata as Metaplex,
+    },
+};
 
 declare_id!("7YXA7HHr9UGXYA3cFC72s9ZUVbHDJbUojGz6puNrDu47");
 
@@ -84,6 +93,67 @@ pub mod loyalty_program {
     }
 }
 
+pub fn init_token(ctx: Context<InitToken>, metadata: InitTokenParams) -> Result<()> {
+    let seeds = &["mint".as_bytes(), ctx.accounts.customer.key().as_ref(), ctx.accounts.merchant.key().as_ref(), &[ctx.bumps.mint]];
+    let signer = [&seeds[..]];
+
+    let token_data: DataV2 = DataV2 {
+        name: metadata.name,
+        symbol: metadata.symbol,
+        uri: metadata.uri,
+        seller_fee_basis_points: 0,
+        creators: None,
+        collection: None,
+        uses: None,
+    };
+
+    let metadata_ctx = CpiContext::new_with_signer(
+        ctx.accounts.token_metadata_program.to_account_info(),
+        CreateMetadataAccountsV3 {
+            payer: ctx.accounts.payer.to_account_info(),
+            update_authority: ctx.accounts.mint.to_account_info(),
+            mint: ctx.accounts.mint.to_account_info(),
+            metadata: ctx.accounts.metadata.to_account_info(),
+            mint_authority: ctx.accounts.mint.to_account_info(),
+            system_program: ctx.accounts.system_program.to_account_info(),
+            rent: ctx.accounts.rent.to_account_info(),
+        },
+        &signer
+    );
+
+    create_metadata_accounts_v3(
+        metadata_ctx,
+        token_data,
+        false,
+        true,
+        None,
+    )?;
+
+    msg!("Token mint created successfully.");
+
+    Ok(())
+}
+
+pub fn mint_token(ctx: Context<MintToken>) -> Result<()> {
+    let seeds = &["mint".as_bytes(), ctx.accounts.customer.key().as_ref(), ctx.accounts.merchant.key().as_ref(), &[ctx.bumps.mint]];
+    let signer = [&seeds[..]];
+
+    mint_to(
+        CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            MintTo {
+                authority: ctx.accounts.mint.to_account_info(),
+                to: ctx.accounts.destination.to_account_info(),
+                mint: ctx.accounts.mint.to_account_info(),
+            },
+            &signer,
+        ),
+        1,
+    )?;
+
+    Ok(())
+}
+
 #[derive(Accounts)]
 pub struct ProcessPayment<'info> {
     #[account(
@@ -144,6 +214,62 @@ pub struct CloseLoyaltyCard<'info> {
     #[account(mut)]
     pub merchant: SystemAccount<'info>,
 }
+
+#[derive(Accounts)]
+#[instruction(
+    params: InitTokenParams
+)]
+pub struct InitToken<'info> {
+    /// CHECK: New Metaplex Account being created
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
+    #[account(
+        init,
+        seeds = [b"mint", customer.key().as_ref(), merchant.key().as_ref()],
+        bump,
+        payer = merchant,
+        mint::decimals = params.decimals,
+        mint::authority = mint,
+    )]
+    pub mint: Account<'info, Mint>,
+  
+    #[account()]
+    pub customer: SystemAccount<'info>,
+    #[account(mut, signer)]
+    pub merchant: SystemAccount<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub token_metadata_program: Program<'info, Metaplex>,
+}
+
+#[derive(Accounts)]
+pub struct MintToken<'info> {
+    #[account(
+        mut,
+        seeds = [b"mint", customer.key().as_ref(), merchant.key().as_ref()],
+        bump,
+        mint::authority = mint,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = merchant,
+        associated_token::mint = mint,
+        associated_token::authority = customer,
+    )]
+    pub destination: Account<'info, TokenAccount>,
+ 
+    #[account()]
+    pub customer: SystemAccount<'info>,
+    #[account(mut, signer)]
+    pub merchant: SystemAccount<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
 
 #[account]
 pub struct LoyaltyCard {
